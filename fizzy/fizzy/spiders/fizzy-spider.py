905ae43682cd2733cf9180b0b43700463348bd34
Antoine Dalmières <antoine.dalmieres@gadz.org>
2019-05-15 22:34:43 +0200

    use the API idiot !

--- fizzy/fizzy/spiders/fizzy-spider.py
+++ fizzy/fizzy/spiders/fizzy-spider.py
@@ -3,33 +3,85 @@ import scrapy
 
 class FizzySpider(scrapy.Spider):
     name = "fizzy"
-    start_urls = ['https://etherscan.io/txs?a=0xe083515d1541f2a9fd0ca03f189f5d321c73b872',]
+    start_urls = ['https://etherscan.io/txs?a=0xe083515d1541f2a9fd0ca03f189f5d321c73b872&ps=100',]
     allowed_domains = ['etherscan.io']
 
     def parse(self, response):
-        for transaction in response.css('span.address-tag a'):
+        for transaction in response.css('span.text-truncate a'):
             if transaction.css('a::attr(href)')[0].extract().startswith("/tx/"):
                 yield response.follow(transaction, callback = self.parse_transaction)
 
-        for page in response.css('div.col-sm-6:nth-child(2) > span:nth-child(1) > a:nth-child(4)'):
+        for page in response.css('ul.pagination:nth-child(1) > li:nth-child(4) > a:nth-child(1)'):
             yield response.follow(page, callback = self.parse)
 
     def parse_transaction(self, response):
         
         #transaction data
-        txHash = response.css('div#tx::text').extract_first().strip()
-        txReceiptStatus = response.css('div.col-sm-9:nth-child(4) > span:nth-child(1) > font:nth-child(1)::text').extract_first().strip()
-        blockHeight = response.css('div.col-sm-9:nth-child(6) > a:nth-child(1)::text').extract_first().strip()
-        timeStamp = response.css('div.col-sm-9:nth-child(8)::text').extract_first().strip()
-        fromAddress = response.css('div.col-sm-9:nth-child(10) > a:nth-child(1)::text').extract_first().strip()
-        toAddress = response.css('a.wordwrap::text').extract_first().strip()
-        value = response.css('#ContentPlaceHolder1_spanValue::text').extract_first().strip()
-        gasLimit = response.css('#ContentPlaceHolder1_spanGasLimit::text').extract_first().strip()
-        gasUsed = response.css('#ContentPlaceHolder1_spanGasUsedByTxn::text').extract_first().strip()
-        gasPrice = response.css('#ContentPlaceHolder1_spanGasPrice::text')[1].extract().strip()
-        actualCost = response.css('#ContentPlaceHolder1_spanTxFee::text')[1].extract().strip()
-        nonce = response.css('div.col-sm-9:nth-child(24) > span:nth-child(1)::text').extract_first().strip()
-        inputData = response.css('#inputdata::text').extract_first()
+        try:
+            txHash = response.css('#spanTxHash::text').extract_first().strip()
+        except:
+            txHash = response.css('div#tx::text').extract_first().strip()
+        
+        try:
+            txReceiptStatus = response.css('.u-label--sm::text').extract_first().strip()
+        except:
+            txReceiptStatus = response.css('div.col-sm-9:nth-child(4) > span:nth-child(1) > font:nth-child(1)::text').extract_first().strip()
+
+        try:    
+            blockHeight = response.css('#ContentPlaceHolder1_maintable > div:nth-child(5) > div:nth-child(2) > a:nth-child(1)::text').extract_first().strip()
+        except:
+            blockHeight = response.css('div.col-sm-9:nth-child(6) > a:nth-child(1)::text').extract_first().strip()
+
+        try:
+            timeStamp = response.css('div.align-items-center:nth-child(7) > div:nth-child(2)::text')[1].extract()
+        except:
+            timeStamp = response.css('div.col-sm-9:nth-child(8)::text').extract_first().strip()
+
+        try:    
+            fromAddress = response.css('#addressCopy::text').extract_first().strip()
+        except:
+            fromAddress = response.css('div.col-sm-9:nth-child(10) > a:nth-child(1)::text').extract_first().strip()
+
+        try:    
+            toAddress = response.css('#contractCopy::text').extract_first().strip()
+        except:
+            toAddress = response.css('a.wordwrap::text').extract_first().strip()
+
+        try:
+            value = response.css('.u-label--value::text').extract_first().strip()
+        except:
+            value = response.css('#ContentPlaceHolder1_spanValue::text').extract_first().strip()
+
+        try:
+            gasLimit = response.css('#ContentPlaceHolder1_spanGasLimit::text').extract_first().strip()
+        except:
+            gasLimit = response.css('#ContentPlaceHolder1_spanGasLimit::text').extract_first().strip()
+
+        try:
+            gasUsed = response.css('#ContentPlaceHolder1_spanGasUsedByTxn::text').extract_first().strip()
+        except:
+            gasUsed = response.css('#ContentPlaceHolder1_spanGasUsedByTxn::text').extract_first().strip()
+
+        try:    
+            gasPrice = response.css('#ContentPlaceHolder1_spanGasPrice::text')[1].extract().strip()
+        except:
+            gasPrice = response.css('#ContentPlaceHolder1_spanGasPrice::text')[1].extract().strip()
+
+        try:
+            actualCost = response.css('#ContentPlaceHolder1_spanTxFee::text')[1].extract().strip()
+        except:
+            actualCost = response.css('#ContentPlaceHolder1_spanTxFee::text')[1].extract().strip()
+
+        try:
+            nonce = response.css('div.mn-3:nth-child(7) > div:nth-child(2) > span:nth-child(1)::text').extract_first().strip()
+        except:
+            nonce = response.css('div.col-sm-9:nth-child(24) > span:nth-child(1)::text').extract_first().strip()
+
+        try:
+            inputData = response.css('#inputdata::text').extract_first()
+        except:
+            inputData = response.css('#inputdata::text').extract_first()
+
         #event data
         eventName = response.css("#funcname_0::text").extract_first()
         eventParam1 = response.css('#chunk_1_1::text').extract_first()
@@ -37,6 +89,8 @@ class FizzySpider(scrapy.Spider):
         eventParam3 = response.css('#chunk_1_3::text').extract_first()
         eventParam4 = response.css('#chunk_1_4::text').extract_first()
         eventParam5 = response.css('#chunk_1_5::text').extract_first()
+
+
 
         yield Transaction(
             txHash = str(txHash),
